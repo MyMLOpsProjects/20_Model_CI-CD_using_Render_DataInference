c59a6c2c60633e8e7c40cf854dfaeee9a9f3b2ce
Pankaj Soni <90625369+pycsr@users.noreply.github.com>
2025-08-10 20:05:43 +0530

    Create drift_check.py

---
+++ drift_check.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from deepchecks.tabular import Dataset
+from deepchecks.tabular.checks import TrainTestDrift
+
+ref = pd.read_csv('ref-data/ref_data.csv')
+live = pd.read_csv('live_inputs.csv')
+
+ref_ds = Dataset(ref)
+live_ds = Dataset(live)
+
+check = TrainTestDrift()
+result = check.run(ref_ds, live_ds)
+
+result.save_as_html('drift_report.html')
+print(result)
