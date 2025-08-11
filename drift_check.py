52a248279d1e566739ba0190eae943eaa650fd08
Pankaj Soni <pycsrbypankaj@gmail.com>
2025-08-11 10:17:34 +0530

    updated drift_check

--- drift_check.py
+++ drift_check.py
@@ -1,16 +1,15 @@
 import pandas as pd
 from deepchecks.tabular import Dataset
-import numpy as np
 from deepchecks.tabular.checks import TrainTestFeatureDrift
 
 ref = pd.read_csv('ref-data/ref_data.csv')
 live = pd.read_csv('live_inputs.csv')
 
-ref_ds = Dataset(ref)
-live_ds = Dataset(live)
+ref_ds = Dataset(ref,label="Name")
+live_ds = Dataset(live,label="Name")
 
 check = TrainTestFeatureDrift()
 result = check.run(ref_ds, live_ds)
 
-result.save_as_html('drift_report.html')
+result.save_as_html('drift_report.html',as_widget=False)
 print(result)
