56a0402f1b22bcc5322c04d9264acb4706be4e3b
Pankaj Soni <pycsrbypankaj@gmail.com>
2025-08-12 10:14:03 +0530

    Updated drift check py file

--- drift_check.py
+++ drift_check.py
@@ -6,9 +6,12 @@ from deepchecks.tabular import Dataset
 ref = pd.read_csv('ref-data/ref_data.csv')
 live = pd.read_csv('live_inputs.csv')
 
+# Detect categorical features from ref dataset
+categorical_features = ref.drop(columns=[ref.columns[-1]]).select_dtypes(include=['object', 'category']).columns.tolist()
+
 # Create Dataset objects
-train_ds = Dataset(ref.iloc[:,:-1])
-test_ds = Dataset(live)
+train_ds = Dataset(ref.iloc[:,:-1],label=None,cat_features=categorical_features)
+test_ds = Dataset(live,label=None,cat_features=categorical_features)
 
 check = TrainTestFeatureDrift()
 result = check.run(train_ds, test_ds)
